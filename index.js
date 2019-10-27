f2555fdf66102a4f43637b9e1c5b4e61694eb11d
Nikolay <enel123@mail.ru>
2019-10-27 22:46:29 +0200

    feat: add functions for changing colors for left and right buttons. Add line width

--- index.js
+++ index.js
@@ -7,12 +7,26 @@ canvas.width = '512';
 changeMatrixSize('4x4');
 
 var coef = 4;
+var colorLeft = '#000';
+var colorRight = '#fff';
+var lineWidth = 1;
 
+let colorInputLeft = document.getElementById("colorLeft");
+let colorInputRight = document.getElementById("colorRight");
+colorLeft = colorInputLeft.value;
+colorRight = colorInputRight.value;
+
+colorInputLeft.addEventListener("input", function() {
+    colorLeft = colorInputLeft.value;
+}, false);
+colorInputRight.addEventListener("input", function() {
+    colorRight = colorInputRight.value;
+}, false);
 
 
 
 document.getElementById('matrixSize').addEventListener('change', function () {
-   changeMatrixSize(this.value);
+    changeMatrixSize(this.value);
 });
 
 function changeMatrixSize(value) {
@@ -31,34 +45,37 @@ function changeMatrixSize(value) {
     }
 }
 
+document.getElementById('lineWidth').addEventListener('change', function () {
+    lineWidth = this.value;
+});
 
 
 function drawFromFile(matrix) {
-  if(matrix!=undefined){
-      let colorDraw;
-      let getColor;
-      let i = 0;
-      let j = 0;
-      if(matrix[0][0].length == 4) {
-          getColor = function () {
-              return `rgba(${matrix[i][j][0]},${matrix[i][j][1]},${matrix[i][j][2]},${255 / matrix[i][j][3]})`;
-          };
-      }else{
-          getColor = function () {
-              return `#${matrix[i][j]}`;
-          };
-      }
-      for (i=0; i<matrix.length; i++){
-          for (j=0; j<matrix[i].length; j++){
-              let x = i*(512/coef);
-              let y = j*(512/coef);
-              colorDraw = getColor();
-              context.fillStyle = colorDraw;
-              context.fillRect(x, y, 512/coef, 512/coef);
-              context.fill();
-          }
-      }
-  }
+    if(matrix!=undefined){
+        let colorDraw;
+        let getColor;
+        let i = 0;
+        let j = 0;
+        if(matrix[0][0].length == 4) {
+            getColor = function () {
+                return `rgba(${matrix[i][j][0]},${matrix[i][j][1]},${matrix[i][j][2]},${255 / matrix[i][j][3]})`;
+            };
+        }else{
+            getColor = function () {
+                return `#${matrix[i][j]}`;
+            };
+        }
+        for (i=0; i<matrix.length; i++){
+            for (j=0; j<matrix[i].length; j++){
+                let x = i*(512/coef);
+                let y = j*(512/coef);
+                colorDraw = getColor();
+                context.fillStyle = colorDraw;
+                context.fillRect(x, y, 512/coef, 512/coef);
+                context.fill();
+            }
+        }
+    }
 }
 
 
@@ -76,6 +93,7 @@ function getJson(fileName) {
     return matrix;
 }
 
+
 canvas.onmousedown = function (event) {
     let color = colorLeft;
 
@@ -86,14 +104,14 @@ canvas.onmousedown = function (event) {
     let y = event.offsetY;
     context.fillStyle = color;
     context.fill();
-    context.fillRect(Math.floor(x/(512/coef))*(512/coef), Math.floor(y/(512/coef))*(512/coef), 512/coef, 512/coef);
+    context.fillRect(Math.floor(x/(512/coef))*(512/coef), Math.floor(y/(512/coef))*(512/coef), 512/coef*lineWidth, 512/coef*lineWidth);
 
     canvas.onmousemove = function (event) {
         let x = event.offsetX;
         let y = event.offsetY;
         context.fillStyle = color;
         context.fill();
-        context.fillRect(Math.floor(x/(512/coef))*(512/coef), Math.floor(y/(512/coef))*(512/coef), 512/coef, 512/coef);
+        context.fillRect(Math.floor(x/(512/coef))*(512/coef), Math.floor(y/(512/coef))*(512/coef), 512/coef*lineWidth, 512/coef*lineWidth);
 
     };
     canvas.onmouseup = function () {
