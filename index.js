f075f9761643f58c09634479549e309e8cddca4e
Nikolay <enel123@mail.ru>
2019-11-05 19:50:41 +0200

    fix: add spaces for readability. Add class 'Painter' for removing 'var' variable

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+class Painter {
+    constructor(coef, colorLeft, colorRight, lineWidth ){
+        this.coef = coef;
+        this.colorLeft = colorLeft;
+        this.colorRight = colorRight;
+        this.lineWidth = lineWidth;
+    }
+}
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
 canvas.height = '512';
 canvas.width = '512';
 
+const painter = new Painter(4, '#000', '#fff', 1);
+
 changeMatrixSize('4x4');
 
-var coef = 4;
-var colorLeft = '#000';
-var colorRight = '#fff';
-var lineWidth = 1;
-
-let colorInputLeft = document.getElementById("colorLeft");
-let colorInputRight = document.getElementById("colorRight");
-colorLeft = colorInputLeft.value;
-colorRight = colorInputRight.value;
+const colorInputLeft = document.getElementById("colorLeft");
+const colorInputRight = document.getElementById("colorRight");
+painter.colorLeft = colorInputLeft.value;
+painter.colorRight = colorInputRight.value;
 
 colorInputLeft.addEventListener("input", function() {
-    colorLeft = colorInputLeft.value;
+    painter.colorLeft = colorInputLeft.value;
 }, false);
 colorInputRight.addEventListener("input", function() {
-    colorRight = colorInputRight.value;
+    painter.colorRight = colorInputRight.value;
 }, false);
-
-
 
 document.getElementById('matrixSize').addEventListener('change', function () {
     changeMatrixSize(this.value);
@@ -31,11 +35,11 @@ document.getElementById('matrixSize').addEventListener('change', function () {
 
 function changeMatrixSize(value) {
     let matrix;
-    if(value == '512x512'){
+    if(value === '512x512'){
         matrix = new Image();
         matrix.onload = function() {
             context.drawImage(matrix, 0, 0 , 512 , 512);
-            coef = 512;
+            painter.coef = 512;
         };
         matrix.src = "src/512x512.png";
     }
@@ -46,39 +50,36 @@ function changeMatrixSize(value) {
 }
 
 document.getElementById('lineWidth').addEventListener('change', function () {
-    lineWidth = this.value;
+    painter.lineWidth = this.value;
 });
 
 
 function drawFromFile(matrix) {
-    if(matrix!=undefined){
+    if(matrix != undefined){
         let colorDraw;
         let getColor;
-        let i = 0;
-        let j = 0;
+
         if(matrix[0][0].length == 4) {
-            getColor = function () {
-                return `rgba(${matrix[i][j][0]},${matrix[i][j][1]},${matrix[i][j][2]},${255 / matrix[i][j][3]})`;
+            getColor = function (i, j) {
+                return `rgba(${matrix[i][j][0]}, ${matrix[i][j][1]}, ${matrix[i][j][2]}, ${255 / matrix[i][j][3]})`;
             };
         }else{
-            getColor = function () {
+            getColor = function (i, j) {
                 return `#${matrix[i][j]}`;
             };
         }
-        for (i=0; i<matrix.length; i++){
-            for (j=0; j<matrix[i].length; j++){
-                let x = i*(512/coef);
-                let y = j*(512/coef);
-                colorDraw = getColor();
+        for (let i = 0; i < matrix.length; i++){
+            for (let j = 0; j < matrix[i].length; j++){
+                let x = i * (512 / painter.coef);
+                let y = j * (512 / painter.coef);
+                colorDraw = getColor(i, j);
                 context.fillStyle = colorDraw;
-                context.fillRect(x, y, 512/coef, 512/coef);
+                context.fillRect(x, y, 512 / painter.coef, 512 / painter.coef);
                 context.fill();
             }
         }
     }
 }
-
-
 
 function getJson(fileName) {
     let matrix;
@@ -88,30 +89,30 @@ function getJson(fileName) {
     oReq.send();
     function reqListener() {
         matrix = JSON.parse(this.responseText);
-        coef = matrix.length;
+        painter.coef = matrix.length;
     }
     return matrix;
 }
 
 
 canvas.onmousedown = function (event) {
-    let color = colorLeft;
+    let color = painter.colorLeft;
 
-    if(event.button==2){
-        color = colorRight;
+    if(event.button == 2){
+        color = painter.colorRight;
     }
     let x = event.offsetX;
     let y = event.offsetY;
     context.fillStyle = color;
     context.fill();
-    context.fillRect(Math.floor(x/(512/coef))*(512/coef), Math.floor(y/(512/coef))*(512/coef), 512/coef*lineWidth, 512/coef*lineWidth);
+    context.fillRect(Math.floor(x / (512 / painter.coef)) * (512 / painter.coef), Math.floor(y / (512 / painter.coef)) * (512 / painter.coef), 512 / painter.coef * painter.lineWidth, 512 / painter.coef * painter.lineWidth);
 
     canvas.onmousemove = function (event) {
         let x = event.offsetX;
         let y = event.offsetY;
         context.fillStyle = color;
         context.fill();
-        context.fillRect(Math.floor(x/(512/coef))*(512/coef), Math.floor(y/(512/coef))*(512/coef), 512/coef*lineWidth, 512/coef*lineWidth);
+        context.fillRect(Math.floor(x / (512 / painter.coef)) * (512 / painter.coef), Math.floor(y / (512 / painter.coef))*(512 / painter.coef), 512 / painter.coef * painter.lineWidth, 512 / painter.coef * painter.lineWidth);
 
     };
     canvas.onmouseup = function () {
